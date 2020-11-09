6be720ed76ad53e09910f9222e9ceb71dc5b9972
JosueRauda7 <katerozarack@gmail.com>
2020-11-09 10:31:55 -0600

    Avance registro

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -1,6 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Login = (props) => {
+  const [formSelected, setFormSelected] = useState("Ingresar");
+  const [user, setUser] = useState(null);
+
+  const toogleForm = () => {
+    if (formSelected === "Ingresar") {
+      setFormSelected("Registrarse");
+    } else {
+      setFormSelected("Ingresar");
+    }
+  };
+
+  const handleEmailChange = (e) => {
+    setUser({
+      ...user,
+      email: e.target.value,
+    });
+  };
+
+  const handlePassswordChange = (e) => {
+    setUser({
+      ...user,
+      password: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    login();
+  };
+
+  const login = async () => {
+    fetch(
+      "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAufGa_G8iSpjSsu08ncty6z0ZfWcIDhSw",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: {
+          email: user.email,
+          password: user.password,
+          returnSecureToken: true,
+        },
+      }
+    )
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err));
+  };
+
   return (
     <section className='our-projects'>
       <div className='skew-arriba'></div>
@@ -16,49 +65,62 @@ const Login = (props) => {
               </div>
               <div className='form-container'>
                 <div className='slide-controls'>
-                  <input type='radio' name='slide' id='login' checked />
-                  <input type='radio' name='slide' id='signup' />
-                  <label for='login' className='slide login'>
+                  <input
+                    type='radio'
+                    name='slide'
+                    id='login'
+                    checked={formSelected === "Ingresar"}
+                  />
+                  <input
+                    type='radio'
+                    name='slide'
+                    id='signup'
+                    checked={formSelected !== "Ingresar"}
+                  />
+                  <label
+                    for='login'
+                    onClick={toogleForm}
+                    className='slide login'>
                     Login
                   </label>
-                  <label for='signup' className='slide signup'>
+                  <label
+                    for='signup'
+                    onClick={toogleForm}
+                    className='slide signup'>
                     Signup
                   </label>
                   <div className='slider-tab'></div>
                 </div>
                 <div className='form-inner'>
-                  <form action='#' className='login'>
+                  <form onSubmit={handleSubmit} className='signup'>
                     <div className='field'>
-                      <input type='text' placeholder='Email Address' required />
-                    </div>
-                    <div className='field'>
-                      <input type='password' placeholder='Password' required />
-                    </div>
-                    <div className='pass-link'>
-                      <a href='#'>Forgot password?</a>
-                    </div>
-                    <div className='field btn'>
-                      <div className='btn-layer'></div>
-                      <input type='submit' value='Login' />
-                    </div>
-                  </form>
-                  <form action='#' class='signup'>
-                    <div className='field'>
-                      <input type='text' placeholder='Email Address' required />
-                    </div>
-                    <div className='field'>
-                      <input type='password' placeholder='Password' required />
+                      <input
+                        type='email'
+                        placeholder='Email Address'
+                        onChange={(e) => handleEmailChange(e)}
+                        required
+                      />
                     </div>
                     <div className='field'>
                       <input
                         type='password'
-                        placeholder='Confirm password'
+                        placeholder='Password'
+                        onChange={(e) => handlePassswordChange(e)}
                         required
                       />
                     </div>
+                    {formSelected === "Registrarse" ? (
+                      <div className='field'>
+                        <input
+                          type='password'
+                          placeholder='Confirm password'
+                          required
+                        />
+                      </div>
+                    ) : null}
                     <div className='field btn'>
                       <div className='btn-layer'></div>
-                      <input type='submit' value='Signup' />
+                      <input type='submit' value={formSelected} />
                     </div>
                   </form>
                 </div>
